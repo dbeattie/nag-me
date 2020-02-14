2a41e4ac9e1e61bdecfc668386e26b47a0aa19cf
Darren Beattie <darren@fettle.ca>
2020-02-13 21:53:37 -0500

    add basic react routes with components folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/goals">Goals</Link>
+        </li>
+        <li>
+          <Link to="/nags">Nags</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      {/*
+        A <Switch> looks through all its children <Route>
+        elements and renders the first one whose path
+        matches the current URL. Use a <Switch> any time
+        you have multiple routes, but you want only one
+        of them to render at a time
+      */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/goals">
+          <Goals />
+        </Route>
+        <Route path="/nags">
+          <Nags />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
 export default App;
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+    </div>
+  );
+}
+
+function Register() {
+  return (
+    <div>
+      <h2>Register</h2>
+    </div>
+  );
+}
+
+function Goals() {
+  return (
+    <div>
+      <h2>Goals</h2>
+    </div>
+  );
+}
+
+function Nags() {
+  return (
+    <div>
+      <h2>Nags</h2>
+    </div>
+  );
+}
