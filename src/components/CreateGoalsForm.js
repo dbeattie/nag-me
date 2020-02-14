aa3a1635fea0b529a3f412b9f2cd83f717d23e65
Don-Stevenson <dsteven1@gmail.com>
2020-02-14 18:39:45 -0500

    add button to goals page

--- src/components/CreateGoalsForm.js
+++ src/components/CreateGoalsForm.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
-      width: 200,
-    },
-  },
+      width: 200
+    }
+  }
 }));
 
-function BasicTextFields() {
-  const classes = useStyles();
-
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label= "Goal Title" variant="outlined" />
-      <TextField id="outlined-basic" label="Nag Title" variant="outlined" />
-    </form>
-  );
-}
-
 export default function CreateGoals() {
+  const classes = useStyles();
   return (
     <div>
-      <h2>Create Goals</h2>
-      <BasicTextFields />
-      <h2>Frequency</h2>
-      {/* insert multiple small icons with days of the week for nags.  Use node-con to hook up */}
-      <h2>Completion Date</h2>
-      {/* Insert Calendar function for date picker - might need to create a new component */}
-      <h3>Date Picker</h3>
+      <form className={classes.root} noValidate autoComplete="off">
+        <h2>Create Goals</h2>
+        <TextField id="outlined-basic" label="Goal Title" variant="outlined" />
+        <TextField id="outlined-basic" label="Nag Title" variant="outlined" />
+        <h2>Frequency</h2>
+        {/* insert multiple small icons with days of the week for nags.  Use node-con to hook up */}
+        <h2>Completion Date</h2>
+        {/* Insert Calendar function for date picker - might need to create a new component */}
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
+     <h3>Date Picker</h3>
     </div>
   );
 }
