f1ea545eba8dde4c3c7421b1c12f87887477d0d8
Don-Stevenson <dsteven1@gmail.com>
2020-02-24 01:17:04 -0500

    chart now made with hooks

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,65 +1,65 @@
-import React, { Component } from "react";
-import { Bar, Line, Pie } from "react-chartjs-2";
+import React, { useState, useEffect } from "react";
+import { Pie } from "react-chartjs-2";
 
-class Chart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chartData: {
-        labels: ["Days Nag Completed", "Days Nags Incomplete"],
-        datasets: [
-          {
-            label: "Nag Completion",
-            data: [80, 20],
-            backgroundColor: ["#FB3640", "#253D5B", "#EFCA08", "#43AA8B"]
+const Chart = props => {
+  const [state, setState] = useState({
+    chartData: {
+      labels: ["Days Nag Completed", "Days Nags Incomplete"],
+      datasets: [
+        {
+          label: "Nag Completion",
+          data: [80, 20],
+          backgroundColor: ["#FB3640", "#253D5B", "#EFCA08", "#43AA8B"]
+        }
+      ]
+    }
+  });
+
+  useEffect(()=> {
+      console.log("it works")
+  })
+
+  return (
+    <div className="chart">
+      <Pie
+        data={state.chartData}
+        options={{
+          title: {
+            display: true,
+            text: `Nag Completion Rate during Learn the Guitar`,
+            fontSize: 20,
+            align: "center",
+            responsive: true
+          },
+        //   plugins: {
+        //     datalabels: {
+        //       color: "#fff",
+        //       anchor: "end",
+        //       align: "start",
+        //       offset: -10,
+        //       borderWidth: 2,
+        //       borderColor: "#fff",
+        //       borderRadius: 25,
+        //       backgroundColor: context => {
+        //         return context.dataset.backgroundColor;
+        //       },
+        //       font: {
+        //         weight: "bold",
+        //         size: 15
+        //       },
+        //       formatter: value => {
+        //         return value + " %";
+        //       }
+        //     }
+        //   },
+          legend: {
+            display: true,
+            position: "bottom"
           }
-        ]
-      }
-    };
-  }
-  render() {
-    return (
-      <div className="chart">
-        <Pie
-          data={this.state.chartData}
-          options={{
-            title: {
-              display: true,
-              text: "Nag Completion Rate during Learn the Guitar",
-              fontSize: 25,
-              align: "center",
-              responsive: true
-            },
-            // plugins: {
-            //   datalabels: {
-            //     color: "#fff",
-            //     anchor: "end",
-            //     align: "start",
-            //     offset: -10,
-            //     borderWidth: 2,
-            //     borderColor: "#fff",
-            //     borderRadius: 25,
-            //     backgroundColor: context => {
-            //       return context.dataset.backgroundColor;
-            //     },
-            //     font: {
-            //       weight: "bold",
-            //       size: 15
-            //     },
-            //     formatter: value => {
-            //       return value + " %";
-            //     }
-            //   }
-            // },
-            legend: {
-              display: true,
-              position: "bottom"
-            }
-          }}
-        />
-      </div>
-    );
-  }
-}
+        }}
+      />
+    </div>
+  );
+};
 
 export default Chart;
--- src/components/NagTracker.js
+++ src/components/NagTracker.js
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from "react";
 import axios from "axios";
 
-import UserContext from '../helpers/UserContext';
-import NagOutlinedCard from './NagCard';
-import Chart from './Chart';
+import UserContext from "../helpers/UserContext";
+import NagOutlinedCard from "./NagCard";
+import Chart from "./Chart";
 
 // //Styling, don't touch
 // const useStyles = makeStyles(theme => ({
@@ -30,77 +30,70 @@ import Chart from './Chart';
 //   },
 // }));
 
-
 export default function NagTracker(props) {
-
   const { user } = useContext(UserContext);
   const [nags, setNags] = React.useState([]);
-  
+
   //Get data from nags table from database
   const fetchNags = async () => {
-  
     const allNags = await axios.get("http://localhost:8001/api/nags");
 
     const nagArray = Object.keys(allNags.data).map(nag => {
       return allNags.data[nag];
     });
-      
+
     const allGoals = await axios.get("http://localhost:8001/api/goals");
 
     const goalArray = Object.keys(allGoals.data).map(goal => {
       return allGoals.data[goal];
-    })
+    });
 
     //Outputs an array of goals based on the logged in user
     const goalsPerUser = goalArray.filter(obj => {
       if (obj.user_id === user) {
-        return obj
+        return obj;
       } else return null;
-    }); 
+    });
 
     //Outputs an array of goal ids
-    const GoalIds = goalsPerUser.map(goal => goal.id)
-  
+    const GoalIds = goalsPerUser.map(goal => goal.id);
+
     //Filters the nagArray for any goal_id's that match
     var filteredNags = nagArray.filter(nag => {
       return GoalIds.includes(nag.goal_id);
     });
-    
+
     setNags(filteredNags);
   };
 
   React.useEffect(() => {
     fetchNags();
-  }, [])
+  }, []);
 
-  console.log("NAG PAGE USER:", user)
+  console.log("NAG PAGE USER:", user);
 
   // UPDATE THE BELOW LOGIC TO HANDLE COMPLETED OR NOT
 
-  const nagCards = nags.map((nag) => {
-
+  const nagCards = nags.map(nag => {
     return (
       <NagOutlinedCard
-      key={nag.id}
-      id={nag.id}
-      name={nag.nag_name}
-      endDate={nag.date}
-      completion={nag.completion}
+        key={nag.id}
+        id={nag.id}
+        name={nag.nag_name}
+        endDate={nag.date}
+        completion={nag.completion}
       />
-      );
-    });
-    console.log("nagsCards are, ", nagCards)
-   
+    );
+  });
+  console.log("nagsCards are, ", nagCards);
+
   return (
-    <div className= "Wrapper"> 
-     <div className = "container d-flex flex-column justify-content-center align-items center">
-       <h1> Nags </h1>
-       <div classname="title text center mb-5"> </div>
-       <Chart  />
+    <div>
+      <h1> Nags </h1>
+      <Chart />
       <section className="goalCards" style={{ maxwidth: "100%" }}>
         {nagCards}
       </section>
-      </div>
     </div>
   );
 }
