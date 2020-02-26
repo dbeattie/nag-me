6890ddffe590c6088ae89f9e721d8bea2148a087
Darren Beattie <darren@fettle.ca>
2020-02-26 12:21:13 -0500

    Update NagCard.js
    Switch is not used.

--- src/components/NagCard.js
+++ src/components/NagCard.js
@@ -6,7 +6,6 @@ import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Slide from '@material-ui/core/Slide';
-import Switch from '@material-ui/core/Switch';
 
 const useStyles = makeStyles({
   root: {
