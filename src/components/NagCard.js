dc8e3aa5b5446b1b2cce290529d4b9f13494eb60
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2020-02-26 18:28:52 -0500

    Just for my self, don't push

--- src/components/NagCard.js
+++ src/components/NagCard.js
@@ -6,17 +6,22 @@ import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Slide from '@material-ui/core/Slide';
-import Switch from '@material-ui/core/Switch';
+import { shadows } from '@material-ui/system';
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275
+    minWidth: 275,
+    background: "linear-gradient(45deg, #009dff 30%, #FF8E53 90%)",
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    marginBottom: "10px",
+    borderRadius: "10px",
   },
   title: {
     fontSize: 14
   },
   pos: {
-    marginBottom: 12
+    marginBottom: 15,
+    color: 'red'
   }
 });
 
