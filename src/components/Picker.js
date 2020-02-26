0a49466c020f76e8f9c99400ade22ea1bd819ad0
Darren Beattie <darren@fettle.ca>
2020-02-26 12:20:51 -0500

    Update Picker.js
    Commented out a console log.

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -15,7 +15,7 @@ export default function MaterialUIPickers(props) {
   const initialDate = props.initialEndDate ? props.initialEndDate : new Date().getTime();
   // console.log("I Am the Initial Date:", initialDate);
   const [selectedDate, setSelectedDate] = useState(initialDate);
-  console.log("I am the default selectedDate:", selectedDate);
+  // console.log("I am the default selectedDate:", selectedDate);
 
   const handleDateChange = date => {
     setSelectedDate(date);
